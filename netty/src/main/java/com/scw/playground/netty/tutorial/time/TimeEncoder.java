2813e558a31be90dc6272f7d8640001311991032
Shi Chengwei <scwgeek@gmail.com>
2019-02-22 18:06:57 +0800

    [netty] modify time encoder, extend encoder from MessageToByteEncoder.

--- netty/src/main/java/com/scw/playground/netty/tutorial/time/TimeEncoder.java
+++ netty/src/main/java/com/scw/playground/netty/tutorial/time/TimeEncoder.java
@@ -2,17 +2,14 @@ package com.scw.playground.netty.tutorial.time;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.MessageToByteEncoder;
 
 /**
  * Created by Shi Chengwei on 22/02/2019.
  */
-public class TimeEncoder extends ChannelOutboundHandlerAdapter {
-    @Override
-    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-        ByteBuf timeBf = ctx.alloc().buffer(4);
-        timeBf.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
-        super.write(ctx, timeBf, promise);
+public class TimeEncoder extends MessageToByteEncoder<Integer> {
+
+    protected void encode(ChannelHandlerContext ctx, Integer time, ByteBuf out) throws Exception {
+        out.writeInt(time);
     }
 }
